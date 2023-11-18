758618219d2e48dbd17c6abbc4ca026d6b93ddfc
nandar510217 <nandarwin0106@gmail.com>
2023-11-18 22:48:58 +0630

    change text

---
+++ app.js
@@ -0,0 +1,12 @@
+let text = document.getElementById('text');
+let changeText = () => {
+    text.innerHTML= 'Hello lady and gentlemen!';
+    text.style.color = 'green';
+}
+
+let changeText1 = () => {
+    text.innerHTML= 'Hello Men and Women!';
+    text.style.color = 'blue';
+}
+
+
