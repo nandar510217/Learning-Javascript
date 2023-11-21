4c47855d27470eaf3340955493e963528fee46f3
nandar510217 <nandarwin0106@gmail.com>
2023-11-21 20:17:30 +0630

    functions and object

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// Change Text
 let text = document.getElementById('text');
 let changeText = () => {
     text.innerHTML= 'Hello lady and gentlemen!';
@@ -9,4 +11,34 @@ let changeText1 = () => {
     text.style.color = 'blue';
 }
 
+//join function
+let names = ["mg mg", "ma ma", "ko ko", "zaw zaw"]
+let joinn = names.join(' - ');
+console.log(joinn);
+
+//map function
+let mapp = names.map((name) => {
+    var look = " is handsome.";
+    if(name == "ma ma" ) {
+        look = " is pretty."
+    }
+    return name + look;
+});
+console.log(mapp);
+
+//filter function
+let filterMaMa = names.filter((name) => { 
+    return name == 'ma ma';
+})
+console.log(names);
+console.log(filterMaMa)
+
+//reduce function
+let prices = [1000, 2000, 1500]
+let totalPrice = prices.reduce((current, num) => {
+    return current + num;
+});
+console.log(totalPrice);
+
+//array spread
 
